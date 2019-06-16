1ec3189fc73995c2f63938525b74cd1b73530cee
Brittany Franklin <brittanyfranklin@users-MacBook-Pro.local>
2019-06-15 20:30:51 -0400

    DB updates

--- create-react-express/client/src/pages/Post.js
+++ create-react-express/client/src/pages/Post.js
@@ -1,7 +1,19 @@
 import React from 'react';
 
 function Post() {
-
+  return (
+    <form>
+      <label>
+        Job Title
+        <input type='text' name='job-title' />
+      </label>
+      <label>
+        Job Description
+        <input type='textarea' name='job-description' />
+      </label>
+      <input type='submit' value='Submit' />
+    </form>
+  )
 }
 
 export default Post;
