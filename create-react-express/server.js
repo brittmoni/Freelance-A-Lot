8d758541e0a3866d21cca027d6e4381e40c90acb
Brittany F <brittfranklin92@gmail.com>
2019-06-23 20:25:43 -0400

    CRUD operators

--- create-react-express/server.js
+++ create-react-express/server.js
@@ -15,15 +15,9 @@ if (process.env.NODE_ENV === "production") {
 
 app.use(router);
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/freelance-a-lot')
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/freelance-a-lot', { useNewUrlParser:true })
   .then(() => console.log('Connected to database'))
   .catch(err => console.log(err));
-
-// Send every other request to the React app
-// Define any API routes before this runs
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
 
 app.listen(PORT, () => {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
