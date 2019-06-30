0d1d323032d6ec907f713c795cbcd536ebd39d8b
Brittany F <brittfranklin92@gmail.com>
2019-06-30 10:36:14 -0400

    Cleaning up code

---
+++ create-react-express/client/build/precache-manifest.569650443a9849729fc22828b4497cb4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "8c26fc8d501fcdba96d6",
+    "url": "/static/js/main.4627bb46.chunk.js"
+  },
+  {
+    "revision": "dfdfd223b0b8c9104070",
+    "url": "/static/js/2.7d0543ab.chunk.js"
+  },
+  {
+    "revision": "8c26fc8d501fcdba96d6",
+    "url": "/static/css/main.4eab82f3.chunk.css"
+  },
+  {
+    "revision": "dfdfd223b0b8c9104070",
+    "url": "/static/css/2.5290102b.chunk.css"
+  },
+  {
+    "revision": "66addf444ec7e0a9ab9c625b5e190148",
+    "url": "/index.html"
+  }
+];
