5eb6ec722d116eda842c20a8ae02173f8277084d
Brittany F <brittfranklin92@gmail.com>
2019-06-30 01:21:16 -0400

    Deploying React app

---
+++ create-react-express/client/build/precache-manifest.e9af8fb9a3b134544a48573298008574.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e5abb0d9d24e0184e551",
+    "url": "/static/js/main.8cc871a1.chunk.js"
+  },
+  {
+    "revision": "950c245f05b8f1a3da62",
+    "url": "/static/js/2.926c40ef.chunk.js"
+  },
+  {
+    "revision": "e5abb0d9d24e0184e551",
+    "url": "/static/css/main.4eab82f3.chunk.css"
+  },
+  {
+    "revision": "950c245f05b8f1a3da62",
+    "url": "/static/css/2.6a8d4023.chunk.css"
+  },
+  {
+    "revision": "694edfa70f5759efa132509c338d37d5",
+    "url": "/index.html"
+  }
+];
--- create-react-express/client/build/service-worker.js
+++ create-react-express/client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7e931f169e7482b9509bec47a8789e92.js"
+  "/precache-manifest.e9af8fb9a3b134544a48573298008574.js"
 );
 
 workbox.clientsClaim();
