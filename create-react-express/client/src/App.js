31c795d6557d1730136caef44e47b5dda4ab3b3f
Brittany F <brittfranklin92@gmail.com>
2019-06-30 03:06:39 -0400

    FS updates

--- create-react-express/client/src/App.js
+++ create-react-express/client/src/App.js
@@ -4,7 +4,6 @@ import "./App.css";
 import Home from './pages/Home';
 import Jobs from './pages/Jobs';
 import Post from './pages/Post';
-import Profile from './pages/Profile';
 import Login from './pages/Login';
 import ProfileAnnabella from'./pages/ProfileAnnabella';
 import ProfileTanya from'./pages/ProfileTanya';
@@ -22,7 +21,6 @@ class App extends Component {
             <Route exact path='/home' component={Home} />
             <Route exact path='/jobs' component={Jobs} />
             <Route exact path='/postjob' component={Post} />
-            <Route exact path='/profile' component={Profile} />
             <Route exact path='/profileAnnabella' component={ProfileAnnabella} />
             <Route exact path='/profileTanya' component={ProfileTanya} />
             <Route exact path='/profileBrittany' component={ProfileBrittany} />
