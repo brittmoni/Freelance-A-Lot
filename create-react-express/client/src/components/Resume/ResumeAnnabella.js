ef3f9d0fd820117bcab35761c89ddbe7192009af
TanyaOdegova <odegovat@yahoo.com>
2019-07-01 00:55:58 -0400

    resume component fixed

--- create-react-express/client/src/components/Resume/ResumeAnnabella.js
+++ create-react-express/client/src/components/Resume/ResumeAnnabella.js
@@ -5,6 +5,8 @@ import 'bootstrap/dist/css/bootstrap.css';
 import Navbar from "../Navbar/Navbar.js"
 import Footer from "../Footer/Footer.js"
 import styles from './Resume.css'
+import {Animated} from "react-animated-css";
+
 const ResumeAnnabella = () => {
     return (
         <div className={styles.uicontainer}>
@@ -14,16 +16,17 @@ const ResumeAnnabella = () => {
             </div>
             <div className='profile-card'>
                 <div className={styles.card}>
+                <div className="animated fadeInRight">
                 <img className="card-img-top rounded-circle z-depth-2" src="https://i.imgur.com/T4EMxKw.jpg"/> 
+                </div>
                         <div className="card-body">
                             <h5 className="card-title">Annabella</h5>
-                            <p className="card-text">Name</p>
                             <p className="card-text">Title</p>
                         </div>
                         <ul className="list-group list-group-flush">
                             <li className="list-group-item">Skills</li>
                             <li className="list-group-item">Work</li>
-                            <li className="list-group-item">Contact informatiom</li>
+                            <li className="list-group-item">Contact information</li>
                         </ul>
                         <div className="card-body">
                         <a href="#" className="btn btn-primary">Contact</a>                        </div>
--- create-react-express/client/src/components/Resume/ResumeNygel.js
+++ create-react-express/client/src/components/Resume/ResumeNygel.js
@@ -5,6 +5,8 @@ import 'bootstrap/dist/css/bootstrap.css';
 import Navbar from "../Navbar/Navbar.js"
 import Footer from "../Footer/Footer.js"
 import styles from './Resume.css'
+import {Animated} from "react-animated-css";
+
 const Resume = () => {
     return (
         <div className={styles.uicontainer}>
@@ -15,16 +17,17 @@ const Resume = () => {
             </div>
             <div className='profile-card'>
                 <div className={styles.card}>
+                <div className="animated fadeInRight">
                 <img className="card-img-top rounded-circle z-depth-2" src="https://i.imgur.com/4B7oHwK.jpg"/> 
+                </div>
                         <div className="card-body">
-                            <h5 className="card-title"></h5>
-                            <p className="card-text">Name</p>
+                            <h5 className="card-title">Nygel</h5>
                             <p className="card-text">Title</p>
                         </div>
                         <ul className="list-group list-group-flush">
                             <li className="list-group-item">Skills</li>
                             <li className="list-group-item">Work</li>
-                            <li className="list-group-item">Contact informatiom</li>
+                            <li className="list-group-item">Contact information</li>
                         </ul>
                         <div className="card-body">
                         <a href="#" className="btn btn-primary">Contact</a>                        </div>
