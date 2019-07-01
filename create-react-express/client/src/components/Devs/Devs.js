839ab2a420483e048a91541d0693f9b8bc585ec1
TanyaOdegova <odegovat@yahoo.com>
2019-07-01 17:49:25 -0400

    annabella's updates for profiles

--- create-react-express/client/src/components/Devs/Devs.js
+++ create-react-express/client/src/components/Devs/Devs.js
@@ -4,104 +4,139 @@ import Card from 'react-bootstrap/Card';
 import CardDeck from 'react-bootstrap/CardDeck';
 import "./Devs.css";
 import {Animated} from "react-animated-css";
-import {Link} from "react-router-dom";
+import {Link} from "react-router-dom"
 
 function Devs (props) {
-    return (
-        <div className="devs">
-        <div>
-            <h3>Browse our highest-rated resources</h3>
-            <hr></hr>
-        </div>
-        <CardDeck className="card animated fadeInUp">
-            <Card className="animated fadeInLeft delay-5s">
-                <Card.Img variant="top"  src="https://i.imgur.com/T4EMxKw.jpg"/>
-                <Card.Body>
-                    <Card.Title>Annabella W.</Card.Title>
-                    <Card.Text>
-                    I'm a creative developer who can take your project idea from scratch and build out a beautiful
-                                    and modern interface that gets results. Whatever the project, my goal is to leave your
-                                    users with the best first impression possible. As a designer, I am here to bring your
-                                    ideas to life. My main focus is to create unique and expressive products that are easy-to-use
-                                    and understand. My favorite tools for the job are Adobe XD, Photoshop, and Illustrator.
-      </Card.Text>
-                </Card.Body>
-                <Card.Footer id="cardfooter">
-                    <small className="text">
-                    <Link className="profile-link" to="/profileAnnabella">Annabella's Portfolio</Link></small>
-                </Card.Footer>
-            </Card>
-            <Card className="animated fadeInLeft delay-5s">
-                <Card.Img variant="top" src="https://i.imgur.com/GnEKrY2.jpg" />
-                <Card.Body>
-                    <Card.Title>Brittany F.</Card.Title>
-                    <Card.Text>
-                    I am a full stack web developer with years of experience designing, developing, and
-                                    maintaining web applications on the cloud.
-                                    I specialize in Node.JS, MySQL, mongoDB, JavaScript, jQuery, Bootstrap, HTML, and CSS. I have never encountered a web development problem I couldn't solve and will always find a solution to your issue.
-                                    My primary focus is to support, service and add value to my customers. I listen and clearly understand my customers issues and technology needs.
-                                    
-        content.{' '}
-                    </Card.Text>
-                </Card.Body>
-                <Card.Footer id="cardfooter">
-                    <small className="text">
-                    <Link className="profile-link" to="/profileBrittany">Brittany's Portfolio</Link></small>
-                </Card.Footer>
-            </Card>
-            <Card>
-                <Card.Img variant="top" src="https://i.imgur.com/4B7oHwK.jpg" />
-                <Card.Body>
-                    <Card.Title>Nygel P.</Card.Title>
-                    <Card.Text>I am a Full-stack Developer. I enjoy taking complex problems and turning them
-                                    into simple and beautiful code. I love structure and logic. I have been coding around
-                                    6 years now and I never stop learning – I love it. I want to share my knowledge with
-                                    other upcoming or current developers. I am a current developer at the Digital Advertising
-                                    Agency based in Charlotte, NC. Where I spend constant hours making the internet a better
-                                    place.                  
-      </Card.Text>
-                </Card.Body>
-                <Card.Footer id="cardfooter">
-                    <small className="text">
-                    <Link className="profile-link" to="/profileNygel">Nygel's Portfolio</Link></small>
-                </Card.Footer>
-            </Card>
-            <Card className="animated fadeInRight delay-5s">
-                <Card.Img variant="top" src="https://i.imgur.com/6Fue2AN.jpg"/>
-                <Card.Body>
-                    <Card.Title>Tanya O.</Card.Title>
-                    <Card.Text>A highly driven, organized, and efficient project manager has a proven track record of quality
-                                    and timely project completions. Experienced in managing and a wide variety of positions
-                                    on various teams throughout numerous projects / project types, Tanya Odegova is focused
-                                    on the big picture milestones while continuous and actively participating in development,
-                                    maintaining feedback and guidance, and pursuing open, appreciable, and gratifying client
-                                    relationships.
-                  
-      </Card.Text>
-                </Card.Body>
-                <Card.Footer id="cardfooter">
-                    <small className="text">
-                    <Link className="profile-link" to="/profileTanya">Tanya's Portfolio</Link></small>
-                </Card.Footer>
-            </Card>
-
-            <Card className="animated fadeInRight delay-5s">
-                <Card.Img variant="top" src="https://i.imgur.com/ubgpU8v.jpg"/>
-                <Card.Body>
-                    <Card.Title>Thibault F.</Card.Title>
-                    <Card.Text>I am a 100% full-time freelancer specializing in creative and efficient designs that work on all devices/platforms. I work with HTML, CSS, PHP, jQuery, MySQL and all Adobe CC apps. Custom WordPressed solutions with enhanced functionality are my main focus.
-                    I use a design/build approach to increase efficiency and decrease development time and costs. Please feel free to invite me to interview on your project. I'm always looking for new opportunities.
-                    
-      </Card.Text>
-                </Card.Body>
-                <Card.Footer id="cardfooter">
-                    <small className="text">
-                    <Link className="profile-link" to="/profileThibault">Thibault's's Portfolio</Link></small>
-                </Card.Footer>
-            </Card>
-        </CardDeck>
-        </div>
-        );
-}
+  return (
+    <div className="devs">
+    <div>
+      <h3>Browse our highest-rated resources</h3>
+      <hr></hr>
+    </div>
+    <CardDeck className="card animated fadeInUp">
+      <Card className="animated fadeInLeft delay-5s">
+        <Card.Img variant="top" src="https://i.imgur.com/T4EMxKw.jpg"/>
+        <Card.Body>
+          <Card.Title>Annabella W.</Card.Title>
+          <Card.Text>
+          <h3> 
+            Expert WordPress Developer
+          </h3>
+          <h2>
+            $97/hr Charlotte, NC
+          </h2>
+          <hr/>
+          <h4>
+          <button type="button" class="btn btn-default">WordPress</button>
+          <button type="button" class="btn btn-default">HTML5</button>
+          <button type="button" class="btn btn-default">HTML</button>
+          <button type="button" class="btn btn-default">CSS</button>
+          </h4>
+          </Card.Text>
+        </Card.Body>
+        <Card.Footer id="cardfooter">
+          <small className="text">
+          <Link className="profile-link" to="/profileannabella">View Profile</Link></small>
+        </Card.Footer>
+      </Card>
+      <Card className="animated fadeInLeft delay-5s">
+        <Card.Img variant="top" src="https://i.imgur.com/GnEKrY2.jpg" />
+        <Card.Body>
+          <Card.Title>Brittany F.</Card.Title>
+          <Card.Text>
+          <h3> 
+            Web Developer
+          </h3>
+          <h2>
+            $70/hr Charlotte, NC
+          </h2>
+          <hr/>
+          <h4>
+          <button type="button" class="btn btn-default">PHP</button>
+          <button type="button" class="btn btn-default">JavaScript</button>
+          <button type="button" class="btn btn-default">HTML5</button>
+          <button type="button" class="btn btn-default">Java</button>
+          </h4>
     
-    export default Devs;
+          </Card.Text>
+        </Card.Body>
+        <Card.Footer id="cardfooter">
+          <small className="text">
+          <Link className="profile-link" to="/profilebrittany">View profile</Link></small>
+        </Card.Footer>
+      </Card>
+      <Card>
+        <Card.Img variant="top" src="https://i.imgur.com/4B7oHwK.jpg" />
+        <Card.Body>
+          <Card.Title>Nygel P.</Card.Title>
+          <Card.Text><h3> 
+            Senior Mobile & Web Developer
+          </h3>
+          <h2>
+            $80/hr Charlotte, NC
+          </h2>
+          <hr/>
+          <h4>
+          <button type="button" class="btn btn-default">iOS Development</button>
+          <button type="button" class="btn btn-default">Android App Development</button>
+          </h4>       
+   </Card.Text>
+        </Card.Body>
+        <Card.Footer id="cardfooter">
+          <small className="text">
+          <Link className="profile-link" to="/profilenygel">View Profile</Link></small>
+        </Card.Footer>
+      </Card>
+      <Card className="animated fadeInRight delay-5s">
+        <Card.Img variant="top" src="https://i.imgur.com/6Fue2AN.jpg"/>
+        <Card.Body>
+          <Card.Title>Tanya O.</Card.Title>
+          <Card.Text>
+          <h3> 
+          Expert Android, iOS, Web Developer          </h3>
+          <h2>
+            $75/hr Charlotte, NC
+          </h2>
+          <hr/>
+          <h4>
+          <button type="button" class="btn btn-default">iOS Development</button>
+          <button type="button" class="btn btn-default">Android App Development</button>
+          </h4>   
+   </Card.Text>
+        </Card.Body>
+        <Card.Footer id="cardfooter">
+          <small className="text">
+          <Link className="profile-link" to="/profiletanya">View Profile</Link></small>
+        </Card.Footer>
+      </Card>
+​
+      <Card className="animated fadeInRight delay-5s">
+        <Card.Img variant="top" src="https://i.imgur.com/ubgpU8v.jpg"/>
+        <Card.Body>
+          <Card.Title>Thibault F.</Card.Title>
+          <Card.Text>
+          <Card.Text><h3> 
+          API Development | Web Development | Automation | PHP & JavaScript
+          </h3>
+          <h2>
+            $75/hr Charlotte, NC
+          </h2>
+          <hr/>
+          <h4>
+          <button type="button" class="btn btn-default">API Development</button>
+          <button type="button" class="btn btn-default">SQL</button>
+          </h4>       
+   </Card.Text>
+   </Card.Text>
+        </Card.Body>
+        <Card.Footer id="cardfooter">
+          <small className="text">
+          <Link className="profile-link" to="/profilethibault">View Profile</Link></small>
+        </Card.Footer>
+      </Card>
+    </CardDeck>
+    </div>
+    );
+}
+
+  export default Devs;
--- create-react-express/client/src/components/Resume/ResumeAnnabella.js
+++ create-react-express/client/src/components/Resume/ResumeAnnabella.js
@@ -1,3 +1,4 @@
+
 import React from 'react';
 import "./Resume.css";
 import faker from 'faker';
@@ -5,37 +6,52 @@ import 'bootstrap/dist/css/bootstrap.css';
 import Navbar from "../Navbar/Navbar.js"
 import Footer from "../Footer/Footer.js"
 import styles from './Resume.css'
-import {Animated} from "react-animated-css";
+import {Animated} from "react-animated-css"
 
 const ResumeAnnabella = () => {
-    return (
-        <div className={styles.uicontainer}>
-            <div className='navbar'>
-            <div className={styles.navbar}>
+  return (
+    <div className={styles.uicontainer}>
+      <div className='navbar'>
+      <div className={styles.navbar}>
+      </div>
+      </div>
+      <div className='profile-card'>
+        <div className={styles.card}>
+        <div className="animated fadeInRight">
+        <img className="card-img-top rounded-circle z-depth-2" src="https://i.imgur.com/T4EMxKw.jpg"/> 
+        </div>
+            <div className="card-body">
+              <h5 className="card-title">Annabella</h5>
+              <p className="card-text">Expert WordPress Developer</p>
+              <p className="card-text">Hey there! I'm Anne, an Austin, TX based developer.
+​
+I am a certified WordPress expert who specializes in PSD to WordPress conversions, WooCommerce and custom theme/plugin development. 
+​
+I have a passion for providing my clients with modern, clean and fully validated sites.
+​
+In 15+ years, I have worked for a variety of clients, from individuals to corporations. I pride myself on providing frequent and professional communication to all of my clients.
+​
+I am a native English speaker.
+​
+I look forward to working with you!
+​
+</p>
             </div>
-            </div>
-            <div className='profile-card'>
-                <div className={styles.card}>
-                <div className="animated fadeInRight">
-                <img className="card-img-top rounded-circle z-depth-2" src="https://i.imgur.com/T4EMxKw.jpg"/> 
-                </div>
-                        <div className="card-body">
-                            <h5 className="card-title">Annabella</h5>
-                            <p className="card-text">Title</p>
-                        </div>
-                        <ul className="list-group list-group-flush">
-                            <li className="list-group-item">Skills</li>
-                            <li className="list-group-item">Work</li>
-                            <li className="list-group-item">Contact information</li>
-                        </ul>
-                        <div className="card-body">
-                        <a href="#" className="btn btn-primary">Contact</a>                        </div>
+            <ul className="list-group list-group-flush">
+              <li className="list-group-item">$97.00
+Hourly rate</li>
+              <li className="list-group-item">$100k+
+Total earned</li>
+              <li className="list-group-item">100 Jobs</li>
+            </ul>
+            <div className="card-body">
+            <a href="#" className="btn btn-primary">Contact</a>            </div>
 </div>
-                </div>
-            </div>
-            );
-        };
-        
-        
-        
-        export default ResumeAnnabella;
+        </div>
+      </div>
+      );
+    };
+    
+    
+    
+    export default ResumeAnnabella;
